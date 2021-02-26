edac12006ac99bd624b31dd67c21e5c2fdc8c8fe
Kerim Satirli <k.satirli@gmail.com>
2021-02-26 16:24:40 +0100

    adds links to provider pages

--- terraform/azure/consul_keys.tf
+++ terraform/azure/consul_keys.tf
@@ -1,3 +1,4 @@
+# see https://registry.terraform.io/providers/hashicorp/consul/latest/docs/data-sources/keys
 data "consul_keys" "agent_count" {
   key {
     name    = "count"
--- terraform/azure/terraform.tf
+++ terraform/azure/terraform.tf
@@ -3,11 +3,13 @@ terraform {
   required_version = "0.14.7"
 
   required_providers {
+    # see https://registry.terraform.io/providers/hashicorp/azurerm/2.49.0
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "2.49.0"
     }
 
+    # see https://registry.terraform.io/providers/hashicorp/consul/2.11.0
     consul = {
       source  = "hashicorp/consul"
       version = "2.11.0"
