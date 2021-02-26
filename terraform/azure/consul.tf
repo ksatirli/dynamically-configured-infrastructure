216e87918fe6ea16e63318af1f3b32b93a292b51
Kerim Satirli <k.satirli@gmail.com>
2021-02-26 15:52:54 +0100

    adds links to provider pages

--- terraform/azure/consul.tf
+++ terraform/azure/consul.tf
@@ -1,3 +1,4 @@
+# see https://registry.terraform.io/providers/hashicorp/consul/2.11.0
 provider "consul" {
   address    = var.consul_address
   scheme     = var.consul_scheme
--- terraform/azure/terraform.tf
+++ terraform/azure/terraform.tf
@@ -1,4 +1,7 @@
 terraform {
+  # see https://www.terraform.io/docs/language/settings/index.html#specifying-a-required-terraform-version
+  required_version = "0.14.7"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
