ccedc197f3de969c36de932feae2a781cb3ffe9c
Kerim Satirli <k.satirli@gmail.com>
2021-03-01 15:08:26 +0100

    linting

--- terraform/kubernetes/kubernetes.tf
+++ terraform/kubernetes/kubernetes.tf
@@ -1,7 +1,7 @@
 # see https://registry.terraform.io/providers/hashicorp/kubernetes/2.0.2
 provider "kubernetes" {
-  host                   = local.config.host
-  client_certificate     = base64decode(local.config.client_certificate)
-  client_key             = base64decode(local.config.client_certificate)
-  cluster_ca_certificate = base64decode(local.config.cluster_ca_certificate)
+  host                   = local.kube_config.host
+  client_certificate     = base64decode(local.kube_config.client_certificate)
+  client_key             = base64decode(local.kube_config.client_certificate)
+  cluster_ca_certificate = base64decode(local.kube_config.cluster_ca_certificate)
 }
