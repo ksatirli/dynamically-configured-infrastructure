039931d6c6fa27fb0e3f094cc2138e4f336a3719
Kerim Satirli <k.satirli@gmail.com>
2021-03-01 13:58:23 +0100

    linting

--- consul/scripts/insert_data.sh
+++ consul/scripts/insert_data.sh
@@ -3,7 +3,7 @@
 # use this script to insert KV pairs for consumption via Terraform
 
 # AKS cluster configuration
-consul kv put "cluster-configuration/agents_count" "3"
+consul kv put "cluster-configuration/agents_count" 3
 
 # generic thresholds
 consul kv put "datadog/thresholds" "{ \"critical\": \"0.75\", \"warning\": \"0.66\", \"recover\": \"0\" }"
