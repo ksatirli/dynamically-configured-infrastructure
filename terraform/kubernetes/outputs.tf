1655157465515a8e28477c61a6f00fdf89833d9a
Kerim Satirli <k.satirli@gmail.com>
2021-03-01 16:20:22 +0100

    adds output for `beacon_url`

--- terraform/kubernetes/outputs.tf
+++ terraform/kubernetes/outputs.tf
@@ -1,5 +1,7 @@
 locals {
   aks_link_base = "${local.portal_prefix}${data.azurerm_kubernetes_cluster.current.id}"
+  app_host = kubernetes_service.beacon.status[0].load_balancer[0].ingress[0].ip
+  app_port = kubernetes_service.beacon.spec[0].port[0].port
 }
 
 output "cluster_url" {
@@ -20,4 +22,9 @@ output "cluster_workloads_url" {
 output "cluster_services_url" {
   description = "Cluster Services and Ingresses"
   value       = "${local.aks_link_base}/servicesAndIngresses"
+}
+
+output "beacon_url" {
+  description = "Beacon"
+  value       = "http://${local.app_host}:${local.app_port}"
 }
