4157cc60c69984033e8c2a942025c727e65ac3af
Kerim Satirli <k.satirli@gmail.com>
2021-03-01 14:06:20 +0100

    write Consul keys

--- terraform/azure/consul_keys.tf
+++ terraform/azure/consul_keys.tf
@@ -6,3 +6,13 @@ data "consul_keys" "agent_count" {
     default = "1"
   }
 }
+
+# see https://registry.terraform.io/providers/hashicorp/consul/latest/docs/resources/keys
+resource "consul_keys" "aks_cluster" {
+  key {
+    # set AKS cluster name as value (by parsing `kube_config_raw` as YAML)
+    # note: this is decidedly NOT a best-practice!
+    path = "cluster-data/aks_name"
+    value = yamldecode(module.aks.kube_config_raw).clusters[0].name
+  }
+}
