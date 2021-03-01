ec695c8db4d99bff23f4a4a98190d6997551eadf
Kerim Satirli <k.satirli@gmail.com>
2021-03-01 22:23:15 +0100

    updates client ID for DD Azure integration

--- terraform/datadog/datadog_azure_integration.tf
+++ terraform/datadog/datadog_azure_integration.tf
@@ -1,6 +1,7 @@
+# see https://registry.terraform.io/providers/DataDog/datadog/latest/docs/resources/integration_azure
 resource "datadog_integration_azure" "platform" {
   tenant_name   = data.azurerm_client_config.current.tenant_id
-  client_id     = data.azurerm_client_config.current.client_id
+  client_id     = azuread_application.datadog.application_id
   client_secret = azuread_service_principal_password.datadog.value
   host_filters  = "environment:testing,app:platform"
 }
