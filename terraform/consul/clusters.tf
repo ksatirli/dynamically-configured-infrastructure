d2027fcb209f4ddace65d93bf90b7697e49ef851
Kerim Satirli <k.satirli@gmail.com>
2021-03-02 21:19:17 +0100

    uses more variables for the cluster

--- terraform/consul/clusters.tf
+++ terraform/consul/clusters.tf
@@ -1,10 +1,9 @@
 # see https://registry.terraform.io/providers/hashicorp/hcs/latest/docs/resources/cluster
 resource "hcs_cluster" "primary" {
   resource_group_name      = data.azurerm_resource_group.platform.name
-  managed_application_name = "${data.azurerm_resource_group.platform.name}-consul-primary"
-  email                    = "community@hashicorp.com"
-  cluster_mode             = "production"
+  managed_application_name = "${data.azurerm_resource_group.platform.name}-${var.hcs_cluster_name_suffix}"
+  email                    = var.hcs_cluster_email
+  cluster_mode             = var.hcs_cluster_cluster_mode
   min_consul_version       = data.hcs_consul_versions.default.recommended
-  vnet_cidr                = "172.25.16.0/24"
-  consul_datacenter        = "dc1"
+  consul_external_endpoint = var.hcs_cluster_consul_external_endpoint
 }
