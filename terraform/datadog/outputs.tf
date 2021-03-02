2fbee27ca3ea9b48fcb992260774a6377ac06de1
Kerim Satirli <k.satirli@gmail.com>
2021-03-02 08:52:23 +0100

    adds outputs for Datadog

---
+++ terraform/datadog/outputs.tf
@@ -0,0 +1,14 @@
+output "integrations" {
+  description = "Integrations"
+  value       = "https://app.datadoghq.com/account/settings#integrations"
+}
+
+output "infrastructure" {
+  description = "Infrastructure"
+  value       = "https://app.datadoghq.com/infrastructure"
+}
+
+output "k8s_services" {
+  description = "K8s Services"
+  value       = "https://app.datadoghq.com/orchestration/overview/service"
+}
