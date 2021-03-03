f3aa5b1db9de5fa490cec5e008d89d80d4742e99
Kerim Satirli <k.satirli@gmail.com>
2021-03-03 10:48:46 +0100

    work up for TFC

--- terraform/consul/terraform.tf
+++ terraform/consul/terraform.tf
@@ -21,4 +21,15 @@ terraform {
       version = "0.2.0"
     }
   }
+
+  //  # see https://www.terraform.io/docs/language/settings/backends/remote.html
+  //  backend "remote" {
+  //    hostname     = "app.terraform.io"
+  //    organization = "a-demo-organization"
+  //
+  //    # see https://www.terraform.io/docs/language/settings/backends/remote.html#workspaces
+  //    workspaces {
+  //      name = "dci-hcs"
+  //    }
+  //  }
 }
