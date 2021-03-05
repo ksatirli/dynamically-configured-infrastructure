c745fe1ba52201de7948640b808657bb470302d0
Kerim Satirli <k.satirli@gmail.com>
2021-03-05 21:04:25 +0100

    linting

--- shipyard/consul/scripts/insert_data.sh
+++ shipyard/consul/scripts/insert_data.sh
@@ -7,7 +7,3 @@ consul kv put "cluster-configuration/agents_count" 3
 
 # generic thresholds
 consul kv put "datadog/thresholds" "{ \"critical\": \"0.75\", \"warning\": \"0.66\", \"recover\": \"0\" }"
-
-# environment-specific thresholds
-consul kv put "datadog/envs/testing/thresholds" "{ \"env\": \"testing\", \"critical\": \"0.70\", \"warning\": \"0.50\", \"recover\": \"0\" }"
-consul kv put "datadog/envs/production/thresholds" "{ \"env\": \"production\", \"critical\": \"0.70\", \"warning\": \"0.50\", \"recover\": \"0\" }"
