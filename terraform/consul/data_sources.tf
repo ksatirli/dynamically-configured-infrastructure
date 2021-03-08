dbd9da9d11246e3d926523f903508debf8937347
Kerim Satirli <k.satirli@gmail.com>
2021-03-08 20:34:56 +0100

    load TF Remote State and use it for data source

--- terraform/consul/data_sources.tf
+++ terraform/consul/data_sources.tf
@@ -1,6 +1,18 @@
+# see https://www.terraform.io/docs/language/state/remote-state-data.html
+data "terraform_remote_state" "upstream" {
+  backend = "remote"
+
+  config = {
+    organization = "a-demo-organization"
+    workspaces = {
+      name = "dci-azure"
+    }
+  }
+}
+
 # see https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/resource_group
 data "azurerm_resource_group" "platform" {
-  name = "platform"
+  name = data.terraform_remote_state.upstream.outputs.resource_group_name
 }
 
 # see https://registry.terraform.io/providers/hashicorp/hcs/latest/docs/data-sources/plan_defaults
